0de2a29cdf5980e2ae4ef3091ba69a812b083792
fish1968 <62780813+fish1968@users.noreply.github.com>
2020-06-14 02:28:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from math import sin, pi, factorial
 
+g = 9.8 #gravitational constant
+
+def T0(length):
+    return 2 * pi * (length/g) ** 0.5
+
 def odd(n):
     #return the mutiplication of odd number from n to 1
     # example:
@@ -44,17 +49,36 @@ def print_coe(term_number):
 def main():
     # The mian part of the function
     while True:
-        angle = eval(input("Enter the angle of the experiment:"))
+        angle = eval(input("Enter the angle of the experiment (0-180 degree):"))
         if angle > 180:
             print("The angle is out of the proper range")
-            print("U finished this program")
+            print("U finished process-1")
             break
         n = eval(input("Enter the term you want:"))
         half_angle = angle*(pi/360)
         print(time(n, half_angle))
-
-
+    
+    while True:
+        length = input("enter the length of the pendulum (q or None to quit):")
+        if length == 'q' or len(length) == 0:
+            print("U finished process-2")
+            break
+        length = eval(length)
+        if length > 0:
+            t = T0(length)
+            ratio = 1
+            for i in range(1,10):
+                ratio += coe(i)
+        print("The period of the pendulum is {}".format(ratio*t))
+    
+    while True:
+        term_number = input("Enter the number of terms you want (>=1):")
+        if term_number == 'q' or len(term_number) == 0:
+            print("U finished process-3")
+            print("U have finisehd the program")
+            break
+        print_coe(term_number)
+        
+                
 if __name__ == '__main__':
     main()
-    term_number = int(input("Enter the number of term you want (>=1):"))
-    print_coe(term_number)
